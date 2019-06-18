6ca0b11999031b3327f4827288d79c1a582b041b
Miroslav <2989578+mdzunic@users.noreply.github.com>
2019-06-18 16:05:15 +0200

    Update qfs-gen.sh
    added condition for generating scss

--- qfs-gen.sh
+++ qfs-gen.sh
@@ -1,20 +1,37 @@
 #! /bin/bash
 echo -n "Enter package name: "
 read name
+read -r -p "Generate scss file? [Y/n] " ynscss
 mkdir $name
 cd $name
-touch $name.{ts,scss,spec.ts}
 echo '
-<template>
-</template>
-
-<script lang="ts">
-    export { default } from "./'$name'";
-</script>
-
-<style lang="scss">
-    @import "./'$name'";
-</style>
+    <template>
+    </template>
+    <script lang="ts">
+        export { default } from "./'$name'";
+    </script>
+    <style lang="scss">
+        @import "./'$name'";
+    </style>
 ' > $name.vue
-echo 'export { default as $name } from "./'$name'.vue"' > index.ts
-echo "Created folder $name with corresponding files: $name.ts, $name.scss, $name.vue, $name.spec.ts, index.ts"
+echo 'export { default as '$name' } from "./'$name'.vue"' > index.ts
+echo '
+    export default class '$name' {
+
+    }
+' > $name.ts
+if [[ $ynscss =~ ^[Yy]$ ]]
+then
+echo '
+.'$name' {
+
+}
+' > $name.scss
+fi
+touch $name.spec.ts
+if [[ $ynscss =~ ^[Yy]$ ]]
+then
+echo -e "Created folder $name with corresponding files: \n$name.ts, \n$name.scss, \n$name.vue, \n$name.spec.ts, \nindex.ts"
+else
+echo -e "Created folder $name with corresponding files: \n$name.ts, \n$name.vue, \n$name.spec.ts, \nindex.ts"
+fi
